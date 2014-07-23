232720cdb1de0bfcc2e761087d2e74e590312bd1
chengfeili <873581167@qq.com>
2014-07-23 21:49:52 +0800

    提价DAIMA

--- ManyCode/ManyCode/ManyCode/MainViewController.m
+++ ManyCode/ManyCode/ManyCode/MainViewController.m
@@ -11,10 +11,13 @@
 #import "PersonalCenterViewController.h"
 #import "ParkDetailViewController.h"
 #import "HomeMapView.h"
+#import "HomeListView.h"
 
-@interface MainViewController ()
+@interface MainViewController ()<UITextFieldDelegate>
 {
-   
+    UITextField *searchText_;
+    HomeMapView *mapView_;
+    HomeListView *listView_;
 }
 
 @end
@@ -62,24 +65,82 @@
     [btnHome setBackgroundColor:[UIColor clearColor]];
     [btnHome setImage:[UIImage imageNamed:@"personButton.png"] forState:UIControlStateNormal];
     [btnHome setImage:[UIImage imageNamed:@"personButton.png"] forState:UIControlStateHighlighted];
-	[btnHome addTarget:self action:@selector(showLeftClick:) forControlEvents:UIControlEventTouchUpInside];
+    [btnHome addTarget:self action:@selector(showLeftClick:) forControlEvents:UIControlEventTouchUpInside];
     if (IOS7) {
         [self.navigationItem setRightBarButtonItemInIOS7:[[UIBarButtonItem alloc] initWithCustomView:btnHome]];
     }
     else {
         self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:btnHome];
     }
-    HomeMapView *mapView = [[HomeMapView alloc] initWithFrame:CGRectMake(0, 0, 320, kCurrentWindowHeight)];
-    [self.view addSubview:mapView];
+    
+    //搜索界面
+    UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 50)];
+    headView.backgroundColor = [UIColor clearColor];
+    UIImageView *backImage = [[UIImageView alloc] initWithFrame:headView.bounds];
+    backImage.backgroundColor = COLOR(197.0, 206.0, 195.0);
+    [headView addSubview:backImage];
+    
+    UIButton *searchButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 5, 25, 25)];
+    searchButton.backgroundColor = [UIColor redColor];
+    [headView addSubview:searchButton];
+    searchText_ = [[UITextField alloc]  initWithFrame:CGRectMake(40, 5, 200, 30)];
+    searchText_.borderStyle = UITextBorderStyleNone;
+    searchText_.delegate = self;
+    [headView addSubview:searchText_];
+    
+    UIButton *changeButton = [[UIButton alloc] initWithFrame:CGRectMake(275, 5, 37, 30)];
+    [changeButton setBackgroundImage:[UIImage imageNamed:@"listButton.png"] forState:UIControlStateNormal];
+    [changeButton setBackgroundImage:[UIImage imageNamed:@"listButtonclick.png"] forState:UIControlStateHighlighted];
+    [changeButton addTarget:self action:@selector(changeClick:) forControlEvents:UIControlEventTouchUpInside];
+    changeButton.tag = 100;
+    [headView addSubview:changeButton];
+    
+    mapView_ = [[HomeMapView alloc] initWithFrame:CGRectMake(0, 0, 320, kCurrentWindowHeight)];
+    [self.view addSubview:mapView_];
+    
+    listView_ = [[HomeListView alloc] initWithFrame:CGRectMake(0, 0, 320, kCurrentWindowHeight)];
+    listView_.hidden = YES;
+    [self.view addSubview:listView_];
+    
+    [self.view addSubview:headView];
 }
 
 
 - (void)showLeftClick:(UIButton *)button
 {
     PersonalCenterViewController *viewCtr = [[PersonalCenterViewController alloc] init];
-//    ParkDetailViewController *viewCtr = [[ParkDetailViewController alloc] init];
+    //    ParkDetailViewController *viewCtr = [[ParkDetailViewController alloc] init];
     [self.navigationController pushViewController:viewCtr animated:YES];
     
+}
+
+
+- (void)changeClick:(UIButton *)button
+{
+    switch (button.tag) {
+        case 100:
+        {
+            button.tag = 101;
+            
+            mapView_.hidden = YES;
+            listView_.hidden = NO;
+            
+            [button setBackgroundImage:[UIImage imageNamed:@"mapButton.png"] forState:UIControlStateNormal];
+            [button setBackgroundImage:[UIImage imageNamed:@"mapButtonclick.png"] forState:UIControlStateHighlighted];
+            break;
+        }
+        case 101:
+        {
+            button.tag = 100;
+            listView_.hidden = YES;
+            mapView_.hidden = NO;
+            [button setBackgroundImage:[UIImage imageNamed:@"listButton.png"] forState:UIControlStateNormal];
+            [button setBackgroundImage:[UIImage imageNamed:@"listButtonclick.png"] forState:UIControlStateHighlighted];
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 #pragma mark - table delegate
