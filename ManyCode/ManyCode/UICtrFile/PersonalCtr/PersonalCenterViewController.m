c63bc8c618a68c6f6cb8d9b377e88b704eed3c81
chengfeili <873581167@qq.com>
2014-07-24 15:15:00 +0800

    提交百度导航的代码

--- ManyCode/ManyCode/UICtrFile/PersonalCtr/PersonalCenterViewController.m
+++ ManyCode/ManyCode/UICtrFile/PersonalCtr/PersonalCenterViewController.m
@@ -56,20 +56,22 @@
     }
     self.view.backgroundColor = [UIColor whiteColor];
     nameArray_ = @[@"我的钱包",@"停车扣费记录",@"快捷支付",@"设置",@"版本更新"];
-    mainTableView_ = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 320, kCurrentWindowHeight )];
+    mainTableView_ = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 320, kCurrentWindowHeight- kTopImageHeight)];
     mainTableView_.delegate = self;
     mainTableView_.dataSource = self;
     mainTableView_.backgroundColor = [UIColor clearColor];
     mainTableView_.backgroundView = nil;
-    [mainTableView_ setTableHeaderView:[self creatHeadView:nil]];
+    [mainTableView_ setTableHeaderView:[self creatHeadView:NO]];
     [mainTableView_ setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     [self.view addSubview:mainTableView_];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loginSuccess:) name:@"loginNotification" object:nil];
 
 }
 
-- (UIView *)creatHeadView:(NSDictionary *)dic
+- (UIView *)creatHeadView:(BOOL )isLogin
 {
-    if (dic) {
+    if (isLogin) {
         UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 200)];
         
         UIImageView *backImage = [[UIImageView alloc] initWithFrame:headView.bounds];
@@ -82,10 +84,10 @@
         
         UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(130, 115, 150, 20)];
         nameLabel.backgroundColor = [UIColor clearColor];
-        nameLabel.text = @"18913394805";
+        nameLabel.text = [[NSUserDefaults standardUserDefaults] objectForKey:kAccountMobile];
         [headView addSubview:nameLabel];
         
-        UILabel *priceLabel = [[UILabel alloc] initWithFrame:CGRectMake(130, 115, 150, 20)];
+        UILabel *priceLabel = [[UILabel alloc] initWithFrame:CGRectMake(130, 140, 150, 20)];
         priceLabel.backgroundColor = [UIColor clearColor];
         priceLabel.text = @"当前余额：34元";
         [headView addSubview:priceLabel];
@@ -142,5 +144,15 @@
     cell.titleLabel.text = nameArray_[indexPath.row];
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     return cell;
+}
+
+
+#pragma mark - notify
+
+- (void)loginSuccess:(NSNotification *)notify
+{
+    if ([[notify name] isEqualToString:@"loginNotification"]) {
+        [mainTableView_ setTableHeaderView:[self creatHeadView:YES]];
+    }
 }
 @end
