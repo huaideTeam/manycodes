da448d7dc921b75c97b2f4dab901a4e83df92ea7
ZhuShouyu <shouyuzhu@hotmail.com>
2014-07-26 14:15:10 +0800

    新增资源文件

---
+++ ManyCode/ManyCode/ThirdPart/Calendar/CalendarView.h
@@ -0,0 +1,15 @@
+//
+//  CalendarView.h
+//  ManyCode
+//
+//  Created by Zhu Shouyu on 7/26/14.
+//  Copyright (c) 2014 lichengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarView : UIView
+
+@property (nonatomic, strong) NSDate *currentChoosedDate;
+
+@end
---
+++ ManyCode/ManyCode/ThirdPart/Calendar/CalendarView.m
@@ -0,0 +1,31 @@
+//
+//  CalendarView.m
+//  ManyCode
+//
+//  Created by Zhu Shouyu on 7/26/14.
+//  Copyright (c) 2014 lichengfei. All rights reserved.
+//
+
+#import "CalendarView.h"
+
+@implementation CalendarView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
