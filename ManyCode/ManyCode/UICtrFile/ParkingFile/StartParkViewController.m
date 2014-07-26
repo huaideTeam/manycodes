fe8caa82c80a81dde8d5538d0011f06fe6a1dd53
chengfeili <873581167@qq.com>
2014-07-26 10:45:29 +0800

    提交代码饼图

--- ManyCode/ManyCode/UICtrFile/ParkingFile/StartParkViewController.m
+++ ManyCode/ManyCode/UICtrFile/ParkingFile/StartParkViewController.m
@@ -9,14 +9,20 @@
 #import "StartParkViewController.h"
 #import "SBTickerView.h"
 #import "SBTickView.h"
-#import "PieChartView.h"
+#import "PCPieChart.h"
 
 #define PIE_HEIGHT 200
 
-@interface StartParkViewController ()<PieChartDelegate>
+@interface StartParkViewController ()
 {
-    NSString *_currentClock;
-    NSMutableArray *_clockTickers;
+    NSInteger hourNumber1_;
+    NSInteger hourNumber2_;
+    NSInteger hourNumber3_;
+    NSInteger hourNumber4_;
+    NSDate *currentDate_;
+    UIScrollView *mainScrollView_;
+    UIButton *openDoorBtn_;
+    
 }
 
 @property (nonatomic, strong)  SBTickerView *clockTickerViewHour1;
@@ -25,11 +31,6 @@
 @property (nonatomic, strong)  SBTickerView *clockTickerViewMinute2;
 @property (nonatomic,strong) NSMutableArray *valueArray;
 @property (nonatomic,strong) NSMutableArray *colorArray;
-@property (nonatomic,strong) NSMutableArray *valueArray2;
-@property (nonatomic,strong) NSMutableArray *colorArray2;
-@property (nonatomic,strong) PieChartView *pieChartView;
-@property (nonatomic,strong) UIView *pieContainer;
-@property (nonatomic,strong) UILabel *selLabel;
 
 @end
 
@@ -59,112 +60,148 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    [self.pieChartView reloadChart];
-}
+
 #pragma mark - use Function
 
 - (void)loadFunctionView
 {
+    
+    currentDate_ = [NSDate date];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    self.title = @"抢车位";
+     self.title = @"对账单";
     
     if (IOS7) {
         [self setExtendedLayoutIncludesOpaqueBars:NO];
         [self setEdgesForExtendedLayout:UIRectEdgeNone];
     }
     
-    _currentClock = @"0000";
-    _clockTickers = [[NSMutableArray alloc] initWithCapacity:12];
-    for (int k = 0; k< 4; k++) {
-        SBTickerView *view = [[SBTickerView alloc] initWithFrame:CGRectMake(40+60*k, 30, 60, 90)];
-        [view setFrontView:[SBTickView tickViewWithTitle:@"0" fontSize:45.]];
-        [self.view addSubview:view];
-        [_clockTickers addObject:view];
+    hourNumber1_ = 0;
+    hourNumber2_ = 0;
+    hourNumber3_ = 0;
+    hourNumber4_ = 0;
+    
+    mainScrollView_ = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, kCurrentWindowHeight - kTopImageHeight)];
+    mainScrollView_.contentSize = CGSizeMake(320, 568);
+    [self.view addSubview:mainScrollView_];
+    
+    
+    UIView *hourView = [[UIView alloc] initWithFrame:CGRectMake(20, 30, 140, 100)];
+    hourView.backgroundColor = COLOR(84, 83, 82);
+    hourView.layer.masksToBounds = YES;
+    hourView.layer.cornerRadius = 3.0;
+    
+    _clockTickerViewHour1 = [[SBTickerView alloc] initWithFrame:CGRectMake(5, 5, 60, 90)];
+    [_clockTickerViewHour1 setFrontView:[SBTickView tickViewWithTitle:@"0" fontSize:45.]];
+    [hourView addSubview:_clockTickerViewHour1];
+    
+    _clockTickerViewHour2 = [[SBTickerView alloc] initWithFrame:CGRectMake(73, 5, 60, 90)];
+    [_clockTickerViewHour2 setFrontView:[SBTickView tickViewWithTitle:@"0" fontSize:45.]];
+    [hourView addSubview:_clockTickerViewHour2];
+    
+    [mainScrollView_ addSubview:hourView];
+    
+    UIView *mintinueView = [[UIView alloc] initWithFrame:CGRectMake(163, 30, 140, 100)];
+    mintinueView.backgroundColor = COLOR(84, 83, 82);
+    mintinueView.layer.masksToBounds = YES;
+    mintinueView.layer.cornerRadius = 3.0;
+    
+    _clockTickerViewMinute1 = [[SBTickerView alloc] initWithFrame:CGRectMake(5, 5, 60, 90)];
+    [_clockTickerViewMinute1 setFrontView:[SBTickView tickViewWithTitle:@"0" fontSize:45.]];
+    [mintinueView addSubview:_clockTickerViewMinute1];
+    
+    _clockTickerViewMinute2 = [[SBTickerView alloc] initWithFrame:CGRectMake(73, 5, 60, 90)];
+    [_clockTickerViewMinute2 setFrontView:[SBTickView tickViewWithTitle:@"0" fontSize:45.]];
+    [mintinueView addSubview:_clockTickerViewMinute2];
+    
+    [mainScrollView_ addSubview:mintinueView];
+    
+    [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(refreshTimeView) userInfo:nil repeats:YES];
+    
+    
+    UILabel * titleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 165, 320, 20)];
+    titleLable.backgroundColor = [UIColor clearColor];
+    titleLable.text = @"账户余额";
+    titleLable.font = FONT(12);
+    titleLable.textAlignment = NSTextAlignmentCenter;
+    [mainScrollView_ addSubview:titleLable];
+    
+    
+    PCPieChart *pieChart = [[PCPieChart alloc] initWithFrame:CGRectMake(0, 250, 320, 150)];
+    [pieChart setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin];
+    [pieChart setDiameter:150];
+    [pieChart setSameColorLabel:NO];
+    [mainScrollView_ addSubview:pieChart];
+    
+    NSMutableArray *components = [NSMutableArray array];
+    for (int i=0; i< 3; i++)
+    {
+        PCPieComponent *component = [PCPieComponent pieComponentWithTitle:@"账户余额" value:25.0];
+        [components addObject:component];
+        
+        if (i==0)
+        {
+            [component setColour:[UIColor whiteColor]];
+        }
+        else if (i==1)
+        {
+            [component setColour:COLOR(64, 163, 104)];
+        }
+        else if (i==2)
+        {
+            [component setColour:COLOR(226, 90, 60)];
+        }
+    }
+    [pieChart setComponents:components];
+    
+    openDoorBtn_ = [[UIButton alloc] initWithFrame:CGRectMake(15, 440, 290, 38)];
+    [openDoorBtn_ addTarget:self action:@selector(openClick:) forControlEvents:UIControlEventTouchUpInside];
+    [openDoorBtn_ setTitle:@"点击开闸" forState:UIControlStateNormal];
+    [openDoorBtn_ setBackgroundImage:[UIImage imageNamed:@"点击开闸未进入常态.png"] forState:UIControlStateNormal];
+    [mainScrollView_ addSubview:openDoorBtn_];
+    
+}
+
+#pragma mark - 点击开闸
+- (void)openClick:(UIButton *)button
+{
+    
+}
+
+#pragma mark - 获取分钟和小时数
+
+- (void)refreshTimeView
+{
+    NSDate *date = [NSDate date];
+    
+    long long timeNum =  [date timeIntervalSinceDate:currentDate_];
+    
+    NSInteger minuteNum =(NSInteger)(timeNum - timeNum%60)/60;
+    
+    NSInteger hourNum = (minuteNum - minuteNum%60)/60;
+    if ((hourNum%10)!=hourNumber2_) { //小时个位数
+        hourNumber2_ = (hourNum%10);
+        [_clockTickerViewHour2 setBackView:[SBTickView tickViewWithTitle:[NSString stringWithFormat:@"%d",hourNumber2_]  fontSize:45.]];
+        [_clockTickerViewHour2 tick:SBTickerViewTickDirectionDown animated:YES completion:nil];
     }
     
-     [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(numberTick:) userInfo:nil repeats:YES];
+    if (((minuteNum - minuteNum%10)/10)!=hourNumber3_) {
+        hourNumber3_ = ((minuteNum - minuteNum%10)/10);
+        [_clockTickerViewMinute1 setBackView:[SBTickView tickViewWithTitle:[NSString stringWithFormat:@"%d",hourNumber3_]  fontSize:45.]];
+        [_clockTickerViewMinute1 tick:SBTickerViewTickDirectionDown animated:YES completion:nil];
+    }
     
-    //饼图
+    if ((minuteNum%10)!=hourNumber4_) { //小时个位数
+        hourNumber4_ = (minuteNum%10);
+        [_clockTickerViewMinute2 setBackView:[SBTickView tickViewWithTitle:[NSString stringWithFormat:@"%d",hourNumber4_]  fontSize:45.]];
+        [_clockTickerViewMinute2 tick:SBTickerViewTickDirectionDown animated:YES completion:nil];
+    }
     
-    self.valueArray = [[NSMutableArray alloc] initWithObjects:
-                       [NSNumber numberWithInt:2],
-                       [NSNumber numberWithInt:3],
-                       [NSNumber numberWithInt:2],
-                       [NSNumber numberWithInt:3],
-                       [NSNumber numberWithInt:3],
-                       [NSNumber numberWithInt:4],
-                       nil];
-    
-    self.colorArray = [NSMutableArray arrayWithObjects:
-                       [UIColor colorWithHue:((0/8)%20)/20.0+0.02 saturation:(0%8+3)/10.0 brightness:91/100.0 alpha:1],
-                       [UIColor colorWithHue:((1/8)%20)/20.0+0.02 saturation:(1%8+3)/10.0 brightness:91/100.0 alpha:1],
-                       [UIColor colorWithHue:((2/8)%20)/20.0+0.02 saturation:(2%8+3)/10.0 brightness:91/100.0 alpha:1],
-                       [UIColor colorWithHue:((3/8)%20)/20.0+0.02 saturation:(3%8+3)/10.0 brightness:91/100.0 alpha:1],
-                       [UIColor colorWithHue:((4/8)%20)/20.0+0.02 saturation:(4%8+3)/10.0 brightness:91/100.0 alpha:1],
-                       [UIColor colorWithHue:((5/8)%20)/20.0+0.02 saturation:(5%8+3)/10.0 brightness:91/100.0 alpha:1],
-                       nil];
-    //add shadow img
-    CGRect pieFrame = CGRectMake((self.view.frame.size.width - PIE_HEIGHT) / 2, 120, PIE_HEIGHT, PIE_HEIGHT);
-    
-    UIImage *shadowImg = [UIImage imageNamed:@"shadow.png"];
-    UIImageView *shadowImgView = [[UIImageView alloc]initWithImage:shadowImg];
-    shadowImgView.frame = CGRectMake(0, pieFrame.origin.y + PIE_HEIGHT*0.92, shadowImg.size.width/2, shadowImg.size.height/2);
-    [self.view addSubview:shadowImgView];
-    
-    self.pieContainer = [[UIView alloc]initWithFrame:pieFrame];
-    self.pieChartView = [[PieChartView alloc]initWithFrame:self.pieContainer.bounds withValue:self.valueArray withColor:self.colorArray];
-    self.pieChartView.delegate = self;
-    [self.pieContainer addSubview:self.pieChartView];
-    [self.pieChartView setAmountText:@"-2456.0"];
-    [self.view addSubview:self.pieContainer];
-    
-    //add selected view
-    UIImageView *selView = [[UIImageView alloc]init];
-    selView.image = [UIImage imageNamed:@"select.png"];
-    selView.frame = CGRectMake((self.view.frame.size.width - selView.image.size.width/2)/2, self.pieContainer.frame.origin.y + self.pieContainer.frame.size.height, selView.image.size.width/2, selView.image.size.height/2);
-    [self.view addSubview:selView];
-    
-    self.selLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 24, selView.image.size.width/2, 21)];
-    self.selLabel.backgroundColor = [UIColor clearColor];
-    self.selLabel.textAlignment = NSTextAlignmentCenter;
-    self.selLabel.font = [UIFont systemFontOfSize:17];
-    self.selLabel.textColor = [UIColor whiteColor];
-    [selView addSubview:self.selLabel];
-    [self.pieChartView setTitleText:@"支出总计"];
-    self.title = @"对账单";
-    
+    if (((hourNum - hourNum%10)/10)!=hourNumber1_) {
+        hourNumber1_ = ((hourNum - hourNum%10)/10);
+        [_clockTickerViewHour1 setBackView:[SBTickView tickViewWithTitle:[NSString stringWithFormat:@"%d",hourNumber1_]  fontSize:45.]];
+        [_clockTickerViewHour1 tick:SBTickerViewTickDirectionDown animated:YES completion:nil];
+    }
+
 }
-
-- (void)numberTick:(id)sender {
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"mmss"];
-    NSString *newClock = [formatter stringFromDate:[NSDate date]];
-    
-    [_clockTickers enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        if (![[_currentClock substringWithRange:NSMakeRange(idx, 1)] isEqualToString:[newClock substringWithRange:NSMakeRange(idx, 1)]]) {
-            [obj setBackView:[SBTickView tickViewWithTitle:[newClock substringWithRange:NSMakeRange(idx, 1)] fontSize:45.]];
-            [obj tick:SBTickerViewTickDirectionDown animated:YES completion:nil];
-        }
-    }];
-    
-    _currentClock = newClock;
-}
-
-
-#pragma mark - 饼图的delegate
-
-- (void)selectedFinish:(PieChartView *)pieChartView index:(NSInteger)index percent:(float)per
-{
-    self.selLabel.text = [NSString stringWithFormat:@"%2.2f%@",per*100,@"%"];
-}
-
-- (void)onCenterClick:(PieChartView *)pieChartView
-{
-     [self.pieChartView reloadChart];
-}
-
-
 @end
